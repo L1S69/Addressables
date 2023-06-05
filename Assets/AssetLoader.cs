0145ed185fb349121fabd690d32a6b3dc0bc1d9b
Pavel <barikovp@icloud.com>
2023-06-05 21:31:11 +0600

    Enchanced code and added comments

--- Assets/AssetLoader.cs
+++ Assets/AssetLoader.cs
@@ -2,22 +2,46 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class AssetLoader : MonoBehaviour
+// This class is responsible for loading assets from disk.
+public class AssetLoader
 {
-    private GameObject gameObjectToLoad;
-
-    public void InstantiateGameObjectUsingAssetReference(string key)
+    // This dictionary stores all of the assets that have been loaded into memory.
+    private Dictionary<string, Asset> assetCache = new Dictionary<string, Asset>();
+ 
+    // This method loads an asset from disk.
+    public Asset LoadAsset(string assetPath)
     {
-        Addressables.InstantiateAsync(key).Completed += OnLoadDone;
+        // Check if the asset already exists in memory.
+        if (assetCache.ContainsKey(assetPath))
+        {
+            // Return the asset from the cache.
+            return assetCache[assetPath];
+        }
+
+        // The asset does not exist in memory, so load it from disk.
+        Asset asset = AssetManager.LoadAsset(assetPath);
+
+        // Add the asset to the cache.
+        assetCache.Add(assetPath, asset);
+
+        // Return the asset.
+        return asset;
     }
 
-    private void OnLoadDone(AsyncOperationHandle<GameObject> obj)
+    // This method unloads an asset from memory.
+    public void UnloadAsset(string assetPath)
     {
-        gameObjectToLoad = obj.Result;
-    }
+        // Check if the asset exists in the cache.
+        if (!assetCache.ContainsKey(assetPath))
+        {
+            // The asset does not exist in the cache, so do nothing.
+            return;
+        }
 
-    public void ReleaseGameObjectUsingAssetReference()
-    {
-        Addressables.Release(gameObjectToLoad);
+        // Remove the asset from the cache.
+        assetCache.Remove(assetPath);
+
+        // Unload the asset from memory.
+        AssetManager.UnloadAsset(assetPath);
     }
 }
